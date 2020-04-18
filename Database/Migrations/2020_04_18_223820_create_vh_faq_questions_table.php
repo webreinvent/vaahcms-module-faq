4e5d526094928be5db6ea2d757abffa05e1fa828
Pradeep <webreinvent@gmail.com>
2020-04-18 13:59:26 +0530

    migration class name changed

--- Database/Migrations/2020_04_18_223820_create_vh_faq_questions_table.php
+++ Database/Migrations/2020_04_18_223820_create_vh_faq_questions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVhLangStringsTable extends Migration
+class CreateVhFaqQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
